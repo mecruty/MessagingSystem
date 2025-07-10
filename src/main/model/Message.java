dd78238520c6964734ef60bf0ba96227e0ad5e02
Terry <terryw13@students.cs.ubc.ca>
2025-07-10 16:35:31 -0700

    Added implementation for Test

--- src/main/model/Message.java
+++ src/main/model/Message.java
@@ -2,18 +2,19 @@ package model;
 
 // Represents a single message, one string
 public class Message {
-    
+
+    private String value;
+
     // EFFECTS: Creates a message with a value.
     public Message(String value) {
-        //stub
+        this.value = value;
     }
 
     public String getValue() {
-        return "";
+        return value;
     }
 
-    public void setValue() {
-        // stub
+    public void setValue(String value) {
+        this.value = value;
     }
-
 }
