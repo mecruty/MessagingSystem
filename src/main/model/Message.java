9d3e856d4be213cbf7ef4c309a4fdaab06d3dac8
Terry <terryw13@students.cs.ubc.ca>
2025-07-10 16:24:58 -0700

    Added specification for message class

--- src/main/model/Message.java
+++ src/main/model/Message.java
@@ -1,5 +1,19 @@
 package model;
 
+// Represents a single message, one string
 public class Message {
+    
+    // EFFECTS: Creates a message with a value.
+    public Message(String value) {
+        //stub
+    }
+
+    public String getValue() {
+        return "";
+    }
+
+    public void setValue() {
+        // stub
+    }
 
 }
