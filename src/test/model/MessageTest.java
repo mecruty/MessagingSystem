3e2a7c9566f9572f1263056b719e2ceb8c072279
Terry <terryw13@students.cs.ubc.ca>
2025-07-10 16:32:14 -0700

    Added tests for Message class

--- src/test/model/MessageTest.java
+++ src/test/model/MessageTest.java
@@ -1,4 +1,20 @@
 package model;
-public class MessageTest {
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MessageTest {
+    private Message testMessage;
+    
+    @BeforeEach
+    public void runBefore() {
+        testMessage = new Message("hello!");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(testMessage.getValue(), "hello!");
+    }
 }
