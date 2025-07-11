377a26de5cdce6541060835943fd239a56b39800
Terry <terryw13@students.cs.ubc.ca>
2025-07-11 15:05:12 -0700

    implemented account

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -1,5 +1,76 @@
 package model;
 
+import java.util.*;
+
+// Represents an account, containing a name, password, and a map of all conversations
 public class Account {
 
+    // EFFECTS: Creates an Account with the given name, password, and no preexisting
+    // conversations
+    public Account(String name, String password) {
+
+    }
+
+    // MODIFIES: this, otherAccount
+    // EFFECTS: Adds a shared empty Conversation to this and acc
+    public void beginConversation(Account acc) {
+
+    }
+
+    // EFFECTS: Returns the given number of messages in the conversaion with acc, starting from the newest message.
+    //          If more messages are requested than actually exist, returns all messages.
+    public List<Message> readConversation(Account acc, int numMessagesToRead) {
+        return null;
+    }
+
+    // EFFECTS: Returns the given number of messages in the conversaion with acc, starting from startIndex.
+    //          If more messages are requested than actually exist, returns all messages.
+    public List<Message> readConversation(Account acc, int numMessagesToRead, int startIndex) {
+        return null;
+    }
+
+    // EFFECTS: Adds message to the conversation between this and acc
+    public void sendMessage(Account acc, Message message) {
+        //stub
+    }
+
+    // EFFECTS: Gets the specific conversation between this and acc.
+    public Conversation getConversationWithAccount(Account acc) {
+        return null;
+    }
+
+    // MODFIES: this, acc
+    // EFFECTS: Sets the conversation between this and acc to con 
+    public void setConversationWithAccount(Account acc, Conversation con) {
+        //stub
+    }
+
+    // EFFECTS: Returns true if given name and password matches this.name and this.password
+    public boolean checkLoginDetails() {
+        return false;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+        //stub
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+    public void setPassword(String password) {
+        //stub
+    }
+
+    public Map<Account, Conversation> getConversations() {
+        return null;
+    }
+
+    public void setConversations(Map<Account, Conversation> conversations) {
+        //stub
+    }
 }
