b5252470078e2d7b7615c340aa54947370433acf
Terry <terryw13@students.cs.ubc.ca>
2025-07-11 12:21:24 -0700

    Specified Conversation

--- src/main/model/Conversation.java
+++ src/main/model/Conversation.java
@@ -1,5 +1,47 @@
 package model;
 
+import java.util.*;
+
+// Represents a conversation having a list of messages and the accounts of the two parties
 public class Conversation {
+
+    // EFFECTS: Creates an empty conversation between two people
+    public Conversation() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Adds a message to the end of the conversation
+    public void addMessage() {
+        //stub
+    }
+
+    // EFFECTS: Returns the given number of messages, starting from the newest
+    //          If more messages are requested then have been sent, will return all messages
+    public List<Message> loadNumMessages(int numToLoad) {
+        return null;
+    }
+
+    // EFFECTS: Returns the given number of messages, starting from the given index
+    //          If more messages are requested then have been sent, will return all messages
+    public List<Message> loadNumMessages(int numToLoad, int startIndex) {
+        return null;
+    }
+
+    public List<Message> getMessages() {
+        return null;
+    }
+
+    public void setMessages(List<Message> messages) {
+       //stub
+    }
+
+    public Account[] getParties() {
+        return null;
+    }
+
+    public void setParties() {
+        //stub
+    }
 
 }
