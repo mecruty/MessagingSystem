bbf7ba5998e5883b94178b1db7435ce898a2afa0
Terry <terryw13@students.cs.ubc.ca>
2025-07-11 20:32:31 -0700

    Updated/Created tests for Message and Conversation

--- src/test/model/ConversationTest.java
+++ src/test/model/ConversationTest.java
@@ -1,5 +1,70 @@
 package model;
 
-public class ConversationTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+public class ConversationTest {
+    private Conversation conversationTest;
+    private Account alice = new Account("Alice", "1234");
+    private Account bob = new Account("Bob", "2345");
+    private Message message1 = new Message(alice, "hi");
+    private Message message2 = new Message(bob, "bye");
+    private Message message3 = new Message(alice, "hello");
+    private Message message4 = new Message(bob, "goodbye");
+
+    @BeforeEach
+    public void runBefore() {
+        conversationTest = new Conversation();
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(conversationTest.getMessages().size(), 0);
+    }
+
+    @Test
+    public void testSetMessages() {
+        conversationTest.addMessage(message1);
+        conversationTest.setMessages(List.of(message2, message3));
+        assertEquals(conversationTest.getMessages(), List.of(message2, message3));
+    }
+
+    @Test
+    public void testAddSingleMessage() {
+        conversationTest.addMessage(message1);
+        assertEquals(conversationTest.getMessages().size(), 1);
+        assertEquals(conversationTest.getMessages().get(0), "hi");
+    }
+
+    @Test
+    public void testAddMultipleMessages() {
+        conversationTest.addMessage(message1);
+        conversationTest.addMessage(message2);
+        assertEquals(conversationTest.getMessages().size(), 2);
+        assertEquals(conversationTest.getMessages().get(0), message1);
+        assertEquals(conversationTest.getMessages().get(1), message2);
+    }
+
+    @Test
+    public void testLoadNumMessages() {
+        conversationTest.addMessage(message1);
+        conversationTest.addMessage(message2);
+        conversationTest.addMessage(message3);
+        conversationTest.addMessage(message4);
+        assertEquals(conversationTest.loadNumMessages(1, 0), message1);
+        assertEquals(conversationTest.loadNumMessages(2, 2), List.of(message3, message4));
+    }
+
+    @Test
+    public void testLoadAllMessages() {
+        conversationTest.addMessage(message1);
+        conversationTest.addMessage(message2);
+        conversationTest.addMessage(message3);
+        conversationTest.addMessage(message4);
+        assertEquals(conversationTest.loadNumMessages(100, 0), List.of(message1, message2, message3, message4));
+    }
 }
--- src/test/model/MessageTest.java
+++ src/test/model/MessageTest.java
@@ -7,16 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageTest {
     private Message testMessage;
-    private Account Alice = new Account("Alice");
+    private Account alice = new Account("Alice", "1234");
+    private Account bob = new Account("Bob", "2345");
     
     @BeforeEach
     public void runBefore() {
-        testMessage = new Message(Alice, "hello!");
+        testMessage = new Message(alice, "hello!");
     }
 
     @Test
     public void testConstructor() {
         assertEquals(testMessage.getValue(), "hello!");
-        assertEquals(testMessage.getSender(), Alice);
+        assertEquals(testMessage.getSender(), alice);
+    }
+
+    @Test
+    public void testSettingFields() {
+        testMessage.setSender(bob);
+        testMessage.setValue("bye!");
+        assertEquals(testMessage.getValue(), "bye!");
+        assertEquals(testMessage.getSender(), bob);
     }
 }
