376a085d7020d8fa397bd93747e85a2f6c9653d8
Terry <terryw13@students.cs.ubc.ca>
2025-07-12 16:17:23 -0700

    Implemented Conversation

--- src/main/model/Conversation.java
+++ src/main/model/Conversation.java
@@ -5,29 +5,38 @@ import java.util.*;
 // Represents a conversation having a list of messages
 public class Conversation {
 
+    private List<Message> messages;
+
     // EFFECTS: Creates an empty conversation between two people
     public Conversation() {
-        // stub
+        messages = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: Adds a message to the end of the conversation
     public void addMessage(Message m) {
-        //stub
+        messages.add(m);
     }
 
     // REQUIRES: numToLoad > 0, startIndex >= 0
     // EFFECTS: Returns the given number of messages, starting from the given index
     //          If more messages are requested then have been sent, will return all messages
     public List<Message> loadNumMessages(int numToLoad, int startIndex) {
-        return null;
+        List<Message> toReturn = new ArrayList<Message>();
+        if (startIndex + numToLoad > messages.size()) {
+            numToLoad = messages.size() - startIndex;
+        }
+        for (int i = 0; i < numToLoad; i++) {
+            toReturn.add(messages.get(startIndex + i));
+        }
+        return toReturn;
     }
 
     public List<Message> getMessages() {
-        return null;
+        return messages;
     }
 
     public void setMessages(List<Message> messages) {
-       //stub
+       this.messages = messages;
     }
 }
