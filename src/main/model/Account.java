6c6acef59a7fa221543e7be1f8ab2497186dc9a0
Terry <terryw13@students.cs.ubc.ca>
2025-07-12 17:01:30 -0700

    Implemented Account

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -5,56 +5,66 @@ import java.util.*;
 // Represents an account, containing a name, password, and a map of all conversations
 public class Account {
 
-    // EFFECTS: Creates an Account with the given name, password, and no preexisting
-    // conversations
-    public Account(String name, String password) {
+    private String name;
+    private String password;
+    private Map<Account, Conversation> conversations;
 
+    // EFFECTS: Creates an Account with the given name, password, and no preexisting conversations
+    public Account(String name, String password) { 
+        this.name = name;
+        this.password = password;
+        conversations = new HashMap<>();
     }
 
     // MODIFIES: this, otherAccount
     // EFFECTS: Adds a shared empty Conversation to this and acc
     public void beginConversation(Account acc) {
-
+        Conversation convo = new Conversation();
+        conversations.put(acc, convo);
+        acc.getConversations().put(this, convo);
     }
 
     // REQUIRES: numMessagesToRead > 0, startIndex >= 0
     // EFFECTS: Returns the given number of messages in the conversaion with acc, starting from startIndex.
     //          If more messages are requested than actually exist, returns all messages.
     public List<Message> readConversation(Account acc, int numMessagesToRead, int startIndex) {
-        return null;
+        Conversation convo = conversations.get(acc);
+        return convo.loadNumMessages(numMessagesToRead, startIndex);
     }
 
     // EFFECTS: Adds message to the conversation between this and acc
-    public void sendMessage(Account acc, Message message) {
-        //stub
+    public void sendMessage(Account acc, Message m) {
+        Conversation convo = conversations.get(acc);
+        convo.addMessage(m);
     }
 
     // EFFECTS: Returns true if given name and password matches this.name and this.password
-    public boolean checkLoginDetails() {
-        return false;
+    public boolean checkLoginDetails(String name, String password) {
+        boolean check = this.name.equals(name) && this.password.equals(password);
+        return check;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
-        //stub
+        this.name = name;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 
     public void setPassword(String password) {
-        //stub
+        this.password = password;
     }
 
     public Map<Account, Conversation> getConversations() {
-        return null;
+        return conversations;
     }
 
     public void setConversations(Map<Account, Conversation> conversations) {
-        //stub
+        this.conversations = conversations;
     }
 }
--- src/test/model/AccountTest.java
+++ src/test/model/AccountTest.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class AccountTest {
     private Account testAccount;
     private Account testAccount2;
+    private Conversation convo = new Conversation();
     private Message message1 = new Message(testAccount, "hi");
     private Message message2 = new Message(testAccount, "bye");
     private Message message3 = new Message(testAccount, "hello");
@@ -32,10 +33,10 @@ public class AccountTest {
     public void testSettingFields() {
         testAccount.setName("Charles");
         testAccount.setPassword("3456");
-        testAccount.setConversations(Map.of(testAccount2, new Conversation()));
+        testAccount.setConversations(Map.of(testAccount2, convo));
         assertEquals(testAccount.getName(), "Charles");
         assertEquals(testAccount.getPassword(), "3456");
-        assertEquals(testAccount.getConversations(), Map.of(testAccount2, new Conversation()));
+        assertEquals(testAccount.getConversations(), Map.of(testAccount2, convo));
     }
 
     @Test
@@ -90,7 +91,7 @@ public class AccountTest {
         testAccount.sendMessage(testAccount2, message2);
         testAccount.sendMessage(testAccount2, message3);
         testAccount.sendMessage(testAccount2, message4);
-        assertEquals(testAccount.readConversation(testAccount2, 1, 0), message1);
+        assertEquals(testAccount.readConversation(testAccount2, 1, 0), List.of(message1));
         assertEquals(testAccount.readConversation(testAccount2, 2, 2), List.of(message3, message4));
     }
 
@@ -113,5 +114,6 @@ public class AccountTest {
     @Test
     public void testCheckLoginDetailsIncorrect() {
         assertFalse(testAccount2.checkLoginDetails("Bob", "1234"));
+        assertFalse(testAccount2.checkLoginDetails("Alice", "2345"));
     }
 }
