17d7f43d872c2d2d67bb445588a252bacc418d32
Terry <terryw13@students.cs.ubc.ca>
2025-07-12 23:31:17 -0700

    Specified PostOffice

--- src/main/ui/PostOffice.java
+++ src/main/ui/PostOffice.java
@@ -1,5 +1,66 @@
 package ui;
 
+import java.util.Scanner;
+
+import model.Account;
+import model.Conversation;
+import model.Message;
+
+// TODO
 public class PostOffice {
 
+    private Scanner sc;
+
+    // EFFECTS: Runs the post office.
+    public PostOffice() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Processes user input
+    private void runPostOffice() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Initializes fields
+    private void setUp() {
+
+    }
+
+    // EFFECT: Processes user input for the login processes
+    private void login() {
+
+    }
+
+    // EFFECT: Returns true if the account name already exists.
+    private boolean matchesExistingAccount() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECT: Creates a new account with a unique name and password
+    private void createAccount() {
+
+    }
+
+    // EFFECT: Processes user input after entering account
+    private void enterAccount() {
+
+    }
+
+    // EFFECT: Loads names of past conversations
+    private void loadConversations() {
+
+    }
+
+    // EFFECT: Selects a conversation to read, processing user input
+    private void selectConversation() {
+
+    }
+
+    // EFFECT: Creates a new conversation
+    private void createNewConversation() {
+
+    }
 }
