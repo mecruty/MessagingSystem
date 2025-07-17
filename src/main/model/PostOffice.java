e034158762445cda13c6bd20bdbbd69e63c87010
Terry <terryw13@students.cs.ubc.ca>
2025-07-17 16:12:32 -0700

    Created specification for PostOffice (replacing old PostOffice in ui)

---
+++ src/main/model/PostOffice.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.*;
+
+// Represents a post office, with a list of accounts
+public class PostOffice {
+
+    // EFFECTS: Creates a post office with no accounts
+    public PostOffice() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Adds an empty account to this
+    public void addAccount(Account acc) {
+
+    }
+
+    // EFFECTS: Returns an account with given name in getAccounts()
+    public Account getAccount() {
+        return null;
+    }
+
+    // EFFECTS: Returns true if getAccounts() contains acc
+    public void contains(Account acc) {
+
+    }
+
+    public Map<String, Account> getAccounts() {
+        return null;
+    }
+
+    public void setAccounts() {
+
+    }
+}
