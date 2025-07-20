da361bbdbff155c0937961c195f2fa3335c5003e
Terry <terryw13@students.cs.ubc.ca>
2025-07-19 19:43:54 -0700

    Implemented JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,6 +1,10 @@
 package persistence;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.json.JSONObject;
 
 import model.PostOffice;
 
@@ -8,27 +12,32 @@ import model.PostOffice;
 // Modelled after json demo program
 public class JsonWriter {
 
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
     // Creates a writer to write to destination file
     public JsonWriter(String destination) {
-
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: Creates writer
     //          Throws FileNotFoundException if destination file cannot found or opened for writing.
     public void openWriter() throws FileNotFoundException {
-
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: Writes JSON representation of PostOffice to file
     public void writeToFile(PostOffice po) {
-
+        JSONObject json = po.toJson();
+        writer.print(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: Closes writer
     public void closeWriter() {
-
+        writer.close();
     }
 }
