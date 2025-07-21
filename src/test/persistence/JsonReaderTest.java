d1febc20255a672fa9d0d56f2cd99a9fde81b3c9
Terry <terryw13@students.cs.ubc.ca>
2025-07-20 23:11:17 -0700

    Fixed bug in JsonReaderTest using the wrong import

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -2,7 +2,7 @@ package persistence;
 
 import model.PostOffice;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
