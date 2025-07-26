fc7b338be8c69442461f122ec257706eda93d387
Terry <terryw13@students.cs.ubc.ca>
2025-07-26 16:50:31 -0700

    Added name to top of AccountUI screen

--- src/main/ui/AccountUI.java
+++ src/main/ui/AccountUI.java
@@ -1,8 +1,11 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 import model.Account;
 import model.PostOffice;
@@ -33,8 +36,10 @@ public class AccountUI extends PostOfficeUI {
     // MODIFIES: this
     // EFFECTS: Creates name panel at top
     private void addNamePanel() {
-        namePanel = new JPanel();
+        namePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        namePanel.setBorder(new EmptyBorder(5, 20, 0, 0));
         name = new JLabel(acc.getName());
+        name.setFont(new Font(Font.SERIF, Font.BOLD, 60));
 
         namePanel.add(name);
         add(namePanel, BorderLayout.NORTH);
